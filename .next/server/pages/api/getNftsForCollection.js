123e30d61d2388a18e6346660be876cb47de405a
gituser6hij <user137@protonmail.com>
2023-03-14 17:06:08 +0700

    cssbackground color3

--- .next/server/pages/api/getNftsForCollection.js
+++ .next/server/pages/api/getNftsForCollection.js
@@ -1,8 +1,8 @@
 "use strict";
 (() => {
 var exports = {};
-exports.id = 928;
-exports.ids = [928];
+exports.id = 791;
+exports.ids = [791];
 exports.modules = {
 
 /***/ 366:
@@ -12,7 +12,7 @@ module.exports = require("alchemy-sdk");
 
 /***/ }),
 
-/***/ 557:
+/***/ 337:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
@@ -23,34 +23,31 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var alchemy_sdk__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(alchemy_sdk__WEBPACK_IMPORTED_MODULE_0__);
 
 async function handler(req, res) {
-    const { address , pageSize , chain , excludeFilter , pageKey  } = JSON.parse(req.body);
-    console.log(chain);
+    const { address , pageKey , pageSize , chain  } = JSON.parse(req.body);
     if (req.method !== "POST") {
         res.status(405).send({
             message: "Only POST requests allowed"
         });
         return;
     }
+    console.log(chain);
     const settings = {
         apiKey: process.env.ALCHEMY_API_KEY,
         network: alchemy_sdk__WEBPACK_IMPORTED_MODULE_0__.Network[chain]
     };
     const alchemy = new alchemy_sdk__WEBPACK_IMPORTED_MODULE_0__.Alchemy(settings);
     try {
-        const nfts = await alchemy.nft.getNftsForOwner(address, {
-            pageSize: pageSize ? pageSize : 100,
-            excludeFilters: excludeFilter && [
-                alchemy_sdk__WEBPACK_IMPORTED_MODULE_0__.NftFilters.SPAM
-            ],
-            pageKey: pageKey ? pageKey : ""
+        const nfts = await alchemy.nft.getNftsForContract(address, {
+            pageKey: pageKey ? pageKey : null,
+            pageSize: pageSize ? pageSize : null
         });
-        const formattedNfts = nfts.ownedNfts.map((nft)=>{
+        const formattedNfts = nfts.nfts.map((nft)=>{
             const { contract , title , tokenType , tokenId , description , media  } = nft;
             return {
                 contract: contract.address,
                 symbol: contract.symbol,
-                collectionName: contract.openSea?.collectionName,
                 media: media[0]?.gateway ? media[0]?.gateway : "https://via.placeholder.com/500",
+                collectionName: contract.openSea?.collectionName,
                 verified: contract.openSea?.safelistRequestStatus,
                 tokenType,
                 tokenId,
@@ -63,6 +60,7 @@ async function handler(req, res) {
             nfts: filteredNfts,
             pageKey: nfts.pageKey
         });
+    // the rest of your code
     } catch (e) {
         console.warn(e);
         res.status(500).send({
@@ -81,7 +79,7 @@ async function handler(req, res) {
 var __webpack_require__ = require("../../webpack-api-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(557));
+var __webpack_exports__ = (__webpack_exec__(337));
 module.exports = __webpack_exports__;
 
 })();
