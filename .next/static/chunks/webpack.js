d775af5639c8e1cf19790c9e9705e916a43a5420
gituser6hij <user137@protonmail.com>
2023-03-14 17:03:16 +0700

    cssbackground color2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -188,7 +188,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d8c355ef10dfac05"; }
+/******/ 		__webpack_require__.h = function() { return "28215a45597305d6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
