ef9a897707b2a3ff365c51b9921736296e1db324
gituser6hij <user137@protonmail.com>
2023-03-14 17:10:27 +0700

    cssbackground color4

--- .next/static/X93z_LRKXtSQiDnxclDs5/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b6c949beae362389.css","static/chunks/pages/index-1ab6f25a2054508d.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
